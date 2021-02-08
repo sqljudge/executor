693a565df2f9412f6703c15a8d11b85b3a22093b
Elman <xineo2530@gmail.com>
2021-02-09 00:56:12 +0300

    docs(grpc): add comments to the exported fields of the server package

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,10 +6,12 @@ import (
 	pb "github.com/sqljudge/executor/internal/executor"
 )
 
+// ExecutorServer implements gRPC server for the executor service
 type ExecutorServer struct {
 	pb.UnimplementedExecutorServer
 }
 
+// Execute executes given SQL query on the corresponding database
 func (s *ExecutorServer) Execute(ctx context.Context, request *pb.Request) (*pb.Response, error) {
 	return &pb.Response{}, nil
 }
